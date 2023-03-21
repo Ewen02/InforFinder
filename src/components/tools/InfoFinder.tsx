40beda482b627179925886ced33fe322c9928220
Ewen02 <ewen.le-quere@epitech.eu>
2023-03-22 00:44:00 +0100

    add: creation de la fonction connexion avec l'api restcountries, récupération des données d'un pays rechercher

--- src/components/tools/InfoFinder.tsx
+++ src/components/tools/InfoFinder.tsx
@@ -7,45 +7,55 @@ export namespace InfoFinder {
 
     export namespace Tvmaze {
 
-        export interface Movie {
-            score: number;
-            show: {
-              id: number;
-              url: string;
-              name: string;
-              genres: string[];
-              language: string;
-              officialSite: string | null;
-              premiered: string;
-              rating: { average: number };
-              runtime: number | null;
-              status: string;
-              summary: string;
-              type: string;
-              updated: number;
-              image: { medium: string; original: string };
-              externals: { tvrage: number | null; thetvdb: number | null; imdb: string | null };
-              averageRuntime: number | null;
-              dvdCountry: string | null;
-              ended: string | null;
-              network: { id: number; name: string; country: { code: string; name: string; timezone: string } | null };
-              webChannel: { id: number; name: string; country: { code: string; name: string; timezone: string } | null } | null;
-              schedule: { time: string; days: string[] };
-              _links: { self: { href: string } | null; previousepisode: { href: string } | null };
-            };
-          }
-          
-          export async function GetMovies(query: string): Promise<Movie[]> {
-            let result: Movie[] = [];
-          
-            await fetch('https://api.tvmaze.com/search/shows?q=' + query)
-              .then((response) => response.json())
-              .then((data) => {
-                result = data;
-              });
-          
-            return result;
-          }
+      export interface Movie {
+          score: number;
+          show: {
+            id: number;
+            url: string;
+            name: string;
+            genres: string[];
+            language: string;
+            officialSite: string | null;
+            premiered: string;
+            rating: { average: number };
+            runtime: number | null;
+            status: string;
+            summary: string;
+            type: string;
+            updated: number;
+            image: { medium: string; original: string };
+            externals: { tvrage: number | null; thetvdb: number | null; imdb: string | null };
+            averageRuntime: number | null;
+            dvdCountry: string | null;
+            ended: string | null;
+            network: { id: number; name: string; country: { code: string; name: string; timezone: string } | null };
+            webChannel: { id: number; name: string; country: { code: string; name: string; timezone: string } | null } | null;
+            schedule: { time: string; days: string[] };
+            _links: { self: { href: string } | null; previousepisode: { href: string } | null };
+          };
+        }
         
+        export async function GetMovies(query: string): Promise<Movie[]> {
+          let result: Movie[] = [];
+        
+          await fetch('https://api.tvmaze.com/search/shows?q=' + query)
+            .then((response) => response.json())
+            .then((data) => {
+              result = data;
+            });
+        
+          return result;
+        }
+        
+    }
+
+    export namespace RestCountries {
+      export async function GetCountrie(query: string) {
+        await fetch('https://restcountries.com/v3.1/name/' + query)
+          .then((data) => data.json())
+          .then((data) => {
+            console.log(data);
+          })
+      }
     }
 }
--- src/pages/InfoCountry/InfoCountry.tsx
+++ src/pages/InfoCountry/InfoCountry.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+import { BasicButton } from "../../components/indexComponents";
+import { InfoFinder } from "../../components/tools/InfoFinder"
+
+
 import "./InfoCountry.scss";
 
 const InforCountry = () => {
+    const [query, setQuery] = useState("");
+    //const [countrie, setCountrie] = useState<InfoFinder.Tvmaze.Movie[]>([]);
+
+    const searchCountries = async () => {
+        const response = await InfoFinder.RestCountries.GetCountrie(query);
+        console.log(response);
+        //setMovies(response);
+    };
+
+    useEffect(() => {
+    }, []);
+
+
     return (
-        <>InfoCountry</>
+        <>
+            <section className="header">
+                <h1>Recherche de Pays</h1>
+            </section>
+            <section className="form-search">
+                <input type="text" placeholder="Search Countrie..." onChange={(e) => setQuery(e.target.value)}/>
+                <BasicButton
+                    text="Find"
+                    onClick={searchCountries}
+                />
+            </section>
+        </>
     )
 }
 
